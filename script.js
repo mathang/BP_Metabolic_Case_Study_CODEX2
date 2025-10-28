644802163882d76a55ef0f138f0210b411f9bde0
mathang <raclark36@gmail.com>
2025-10-28 13:47:41 +1000

    Comment out correctAnswer fields in script.js
    Comment out correct answers for slides 6 to 8 in the quiz.

--- script.js
+++ script.js
@@ -66,7 +66,7 @@ const SLIDE_DECK_CONTENT = [
       "Triglycerides",
       "Sedentary Profession"
     ],
-    "correctAnswer": ["Age", "Male gender"],
+    //"correctAnswer": ["Age", "Male gender"],
     //"feedback": "Age and male gender are non-modifiable risk factors. In contrast, the others are modifiable, which means they can be addressed or brought to within normal ranges through diet, exercise, medications and/or lifestyle changes. In particular it is important to note, that even though his profession is sedentary, we can modify this risk factor by including regular activity breaks."
   },
   {
@@ -75,7 +75,7 @@ const SLIDE_DECK_CONTENT = [
     "title": "Calculating Calvin's BMI",
     "question": "In order to treat Calvin effectively we must first do a preliminary clinical assessment to identify his risk factors. Firstly, we must calculate his BMI based on his: \nHeight: 186 cm\nWeight: 100 kg\n\nType your answer in the text box below (keep all working to 1 decimal place).",
     "label": "Enter Answer:",
-    "correctAnswer": "28.9",
+    //"correctAnswer": "28.9",
     //"feedback": "Click 'Next' to continue."
   },
   {
@@ -84,7 +84,7 @@ const SLIDE_DECK_CONTENT = [
     "title": "The Cholesterol Test",
     "question": "Calvin’s current physical activity includes lifting weights (if he is somewhere where there is a gym) and a game of golf each week. Family history: father alive at 77 years, but had a heart attack from 45 years of age. No other family history of note. The aim of this case study is for you, as an exercise professional, to identify safe and effective strategies that will help Calvin reduce his blood pressure to a normal level, and improve his waist measurement and overall cardiovascular health.Before we move on, we must consider an important factor in Calvin’s assessment. Is there a problem with having cholesterol checked at a health booth and if so what is the most likley effect? Select from the key words.",
     "options": ["Yes - False positive", "No", "Yes - False negative"],
-    "correctAnswer": ["False positive"],
+    // "correctAnswer": ["Yes - False positive"],
     // "feedback": "Potentially, yes. If the person is not fasting or has a fatty residue from a hand cream on their hands, this test will not give you a true indication of the cholesterol level. For example, the patient may give a false positive result, that is, a result higher than usual."
   },
   {
