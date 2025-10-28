271623a6394d4cdfdd42e45241b3b57c548f4fc8
mathang <raclark36@gmail.com>
2025-10-28 13:52:23 +1000

    Update script.js

--- script.js
+++ script.js
@@ -66,8 +66,7 @@ const SLIDE_DECK_CONTENT = [
       "Triglycerides",
       "Sedentary Profession"
     ],
-    //"correctAnswer": ["Age", "Male gender"],
-    //"feedback": "Age and male gender are non-modifiable risk factors. In contrast, the others are modifiable, which means they can be addressed or brought to within normal ranges through diet, exercise, medications and/or lifestyle changes. In particular it is important to note, that even though his profession is sedentary, we can modify this risk factor by including regular activity breaks."
+    "feedback": "Age and male gender are non-modifiable risk factors. In contrast, the others are modifiable, which means they can be addressed or brought to within normal ranges through diet, exercise, medications and/or lifestyle changes. In particular it is important to note, that even though his profession is sedentary, we can modify this risk factor by including regular activity breaks."
   },
   {
     "slideNumber": 7,
@@ -75,8 +74,7 @@ const SLIDE_DECK_CONTENT = [
     "title": "Calculating Calvin's BMI",
     "question": "In order to treat Calvin effectively we must first do a preliminary clinical assessment to identify his risk factors. Firstly, we must calculate his BMI based on his: \nHeight: 186 cm\nWeight: 100 kg\n\nType your answer in the text box below (keep all working to 1 decimal place).",
     "label": "Enter Answer:",
-    //"correctAnswer": "28.9",
-    //"feedback": "Click 'Next' to continue."
+    "feedback": "Click 'Next' to continue."
   },
   {
     "slideNumber": 8,
@@ -84,8 +82,7 @@ const SLIDE_DECK_CONTENT = [
     "title": "The Cholesterol Test",
     "question": "Calvin’s current physical activity includes lifting weights (if he is somewhere where there is a gym) and a game of golf each week. Family history: father alive at 77 years, but had a heart attack from 45 years of age. No other family history of note. The aim of this case study is for you, as an exercise professional, to identify safe and effective strategies that will help Calvin reduce his blood pressure to a normal level, and improve his waist measurement and overall cardiovascular health.Before we move on, we must consider an important factor in Calvin’s assessment. Is there a problem with having cholesterol checked at a health booth and if so what is the most likley effect? Select from the key words.",
     "options": ["Yes - False positive", "No", "Yes - False negative"],
-    // "correctAnswer": ["Yes - False positive"],
-    // "feedback": "Potentially, yes. If the person is not fasting or has a fatty residue from a hand cream on their hands, this test will not give you a true indication of the cholesterol level. For example, the patient may give a false positive result, that is, a result higher than usual."
+    "feedback": "Potentially, yes. If the person is not fasting or has a fatty residue from a hand cream on their hands, this test will not give you a true indication of the cholesterol level. For example, the patient may give a false positive result, that is, a result higher than usual."
   },
   {
     "slideNumber": 9,
@@ -96,7 +93,6 @@ const SLIDE_DECK_CONTENT = [
       "List one short term goal below:",
       "List one LONG term goal below:"
     ],
-    "correctAnswer": null,
     "feedback": "Your short term goal for his blood pressure should look something like this: To reduce his resting blood pressure to less than 150/85mmHg within 8-12 weeks. \n\nYour long term goal for regular exercise should look something like this: To have Calvin completing any form of exercise for >40 minutes five days per week in 6 months from now.\n\nIf your goals look similar to these, congratulations, you are correct!"
   },
   {
